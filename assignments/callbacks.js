2134769ba97a42f751438e6dacdea508e4f0393f
Lucas Bazemore <lucasabazemore@gmail.com>
2019-07-23 12:42:23 -0500

    completed the callback section

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,10 +2,10 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
+/*
 
-  //Given this problem: 
-  
+  //Given this problem:
+
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
@@ -17,39 +17,98 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     return cb(arr[0]);
   }
 
-  // Function invocation 
+  // Function invocation
   firstItem(items, function(first) {
     console.log(first)
   });
 
 */
 
-
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+    // getLength passes the length of the array into the callback.
+    return cb(arr.length);
 }
+
+//invocation
+getLength(items, length => {
+    console.log(length);
+});
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+    // last passes the last item of the array into the callback.
+    return cb(arr[arr.length - 1]);
 }
+
+//invocation
+last(items, lastItem => {
+    console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+    // sumNums adds two numbers (x, y) and passes the result to the callback.
+    return cb(x + y);
 }
+
+//invocation
+sumNums(3, 4, summed => {
+    console.log(summed);
+});
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+    // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb(x * y);
 }
 
+//invocation
+multiplyNums(3, 4, multiplied => {
+    console.log(multiplied);
+});
+
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+    // contains checks if an item is present inside of the given array/list.
+    // Pass true to the callback if it is, otherwise pass false.
+    return cb(list.some(itemInList => item == itemInList));
 }
+
+contains('Pencil', items, truthy => {
+    console.log(truthy);
+});
 
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+    // removeDuplicates removes all duplicate values from the given array.
+    // Pass the duplicate free array to the callback function.
+    // Do not mutate the original array.
+    return cb([...new Set(array)]);
 }
+
+let newItems = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    5,
+    4,
+    3,
+    2,
+    1,
+    1,
+    2,
+    3,
+    4,
+    7,
+    8,
+    9,
+    8,
+    6,
+    5,
+    3,
+    2,
+];
+
+//invocation
+removeDuplicates(newItems, deduped => {
+    console.log(deduped.sort());
+});
